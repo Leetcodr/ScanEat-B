275e61adf45dc0d71d5881c0ed5840a70b422187
Leetcodr <114878725+Leetcodr@users.noreply.github.com>
2023-11-22 21:19:28 +0530

    Delete src directory

--- src/components/ManagerPage/ManagerDashboard/DoughnutChart/DoughnutChart.jsx
+++
@@ -1,8 +0,0 @@
-import { Doughnut } from "react-chartjs-2";
-import {Chart as Chartjs} from "chart.js/auto";
-
-const DoughnutChart=({chartData})=>{
-    return <Doughnut data={chartData}/>
-}
-
-export default DoughnutChart;
