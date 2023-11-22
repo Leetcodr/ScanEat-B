3d64829340a1971db375292bddee1ca363a5e3d5
Leetcodr <114878725+Leetcodr@users.noreply.github.com>
2023-11-23 00:19:14 +0530

    Update analyticsModel.js all lowercase

--- Server/models/analyticsModel.js
+++ Server/models/analyticsModel.js
@@ -7,10 +7,10 @@ class AnalyticsModel {
       SELECT
         DATE(O.order_time) AS sale_date,
         SUM(MI.price * OI.quantity) AS daily_sales
-      FROM Orders AS O
-      JOIN OrderItems AS OI ON O.order_id = OI.order_id
-      JOIN MenuItems AS MI ON OI.item_id = MI.item_id
-      JOIN Tables AS T ON O.table_id = T.table_id
+      FROM orders AS O
+      JOIN orderitems AS OI ON O.order_id = OI.order_id
+      JOIN menuitems AS MI ON OI.item_id = MI.item_id
+      JOIN tables AS T ON O.table_id = T.table_id
       WHERE DATE(O.order_time) = CURDATE()
       GROUP BY sale_date
       ORDER BY sale_date
@@ -27,10 +27,10 @@ class AnalyticsModel {
         MI.src,
         MI.price,
         SUM(OI.quantity) AS total_quantity_sold
-      FROM OrderItems AS OI
-      JOIN MenuItems AS MI ON OI.item_id = MI.item_id
-      JOIN Orders AS O ON O.order_id = OI.order_id
-      JOIN Tables AS T ON O.table_id = T.table_id
+      FROM orderitems AS OI
+      JOIN menuitems AS MI ON OI.item_id = MI.item_id
+      JOIN orders AS O ON O.order_id = OI.order_id
+      JOIN tables AS T ON O.table_id = T.table_id
       GROUP BY MI.item_id
       ORDER BY total_quantity_sold DESC
       LIMIT 10;
@@ -38,21 +38,21 @@ class AnalyticsModel {
     connection.query(mostSoldItemsQuery, callback);
   }
 
-  static getMostSoldOrders(callback) {
-    const mostSoldOrdersQuery = `
+  static getMostSoldorders(callback) {
+    const mostSoldordersQuery = `
       SELECT
         O.order_id,
         T.table_number,
         SUM(MI.price * OI.quantity) AS total_order_sales
-      FROM Orders AS O
-      JOIN OrderItems AS OI ON O.order_id = OI.order_id
-      JOIN MenuItems AS MI ON OI.item_id = MI.item_id
-      JOIN Tables AS T ON O.table_id = T.table_id
+      FROM orders AS O
+      JOIN orderitems AS OI ON O.order_id = OI.order_id
+      JOIN menuitems AS MI ON OI.item_id = MI.item_id
+      JOIN tables AS T ON O.table_id = T.table_id
       GROUP BY O.order_id
       ORDER BY total_order_sales DESC
       LIMIT 10;
     `;
-    connection.query(mostSoldOrdersQuery, callback);
+    connection.query(mostSoldordersQuery, callback);
   }
 
   static getMostOrderedTable(callback) {
@@ -60,10 +60,10 @@ class AnalyticsModel {
       SELECT
         T.table_number,
         SUM(MI.price * OI.quantity) AS total_order_sales
-      FROM Orders AS O
-      JOIN OrderItems AS OI ON O.order_id = OI.order_id
-      JOIN Tables AS T ON O.table_id = T.table_id
-      JOIN MenuItems AS MI ON OI.item_id = MI.item_id
+      FROM orders AS O
+      JOIN orderitems AS OI ON O.order_id = OI.order_id
+      JOIN tables AS T ON O.table_id = T.table_id
+      JOIN menuitems AS MI ON OI.item_id = MI.item_id
       GROUP BY T.table_number
       ORDER BY total_order_sales DESC
       LIMIT 5;
@@ -77,9 +77,9 @@ class AnalyticsModel {
         DATE(O.order_time) AS order_date,
         COUNT(OI.order_id) AS total_orders,
         SUM(MI.price * OI.quantity) AS total_price
-      FROM Orders AS O
-      JOIN OrderItems AS OI ON O.order_id = OI.order_id
-      JOIN MenuItems AS MI ON OI.item_id = MI.item_id
+      FROM orders AS O
+      JOIN orderitems AS OI ON O.order_id = OI.order_id
+      JOIN menuitems AS MI ON OI.item_id = MI.item_id
       WHERE DATE(O.order_time) = CURDATE()
       GROUP BY order_date;
     `;
